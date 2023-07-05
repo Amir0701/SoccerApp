70cd6538f98d3576f61914ac391da1b0e5922f2d
Amir0701 <amirusmonov2002@gmail.com>
2023-07-05 17:52:21 +0300

    WebViewFragment added

---
+++ app/src/main/java/com/example/soccerapp/ui/view/WebViewFragment.kt
@@ -0,0 +1,18 @@
+package com.example.soccerapp.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.soccerapp.R
+
+class WebViewFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_web_view, container, false)
+    }
+}
