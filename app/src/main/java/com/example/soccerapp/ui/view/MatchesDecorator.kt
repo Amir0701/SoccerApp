61b29084928f3bea0ec3e48a129e049961b16766
Amir0701 <amirusmonov2002@gmail.com>
2023-07-07 01:23:48 +0300

    add margin when orientation is landscape

--- app/src/main/java/com/example/soccerapp/ui/view/MatchesDecorator.kt
+++ app/src/main/java/com/example/soccerapp/ui/view/MatchesDecorator.kt
@@ -1,5 +1,6 @@
 package com.example.soccerapp.ui.view
 
+import android.content.res.Configuration
 import android.graphics.Rect
 import android.util.TypedValue
 import android.view.View
@@ -20,6 +21,12 @@ class MatchesDecorator : RecyclerView.ItemDecoration() {
             margin,
             view.resources.displayMetrics
         ).toInt()
+
+        if(view.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            if(parent.getChildAdapterPosition(view) == parent.adapter!!.itemCount - 2){
+                outRect.bottom = space
+            }
+        }
 
         if(parent.getChildAdapterPosition(view) == parent.adapter!!.itemCount - 1){
             outRect.bottom = space
